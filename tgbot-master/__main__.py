fd78325ebe1c83a68ce7cbb560c6cd57dbf55f16
Shamil Habeeb <72779328+shamilhabeebnelli@users.noreply.github.com>
2021-04-11 12:30:46 +0530

    Update __main__.py

--- tgbot-master/__main__.py
+++ tgbot-master/__main__.py
@@ -19,15 +19,9 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 
-എന്നെ ഉണ്ടാക്കിയത് എന്റെ മൊതലാളി @devil_spy ആണ് ഹായ് {}, എന്റെ പേര് {}! ഞാൻ [ഇദ്ദേഹം](tg://user?id={}) നോക്കി നടത്തുന്ന ഒരു അടിപൊളി അഡ്മിൻ ബോട്ടാണ്.
+എന്നെ ഉണ്ടാക്കിയത് എന്റെ മൊതലാളി @shamilnelli യും @arumillathavann ഉം ആണ് ഹായ് {}, എന്റെ പേര് {}! ഞാൻ [ഇദ്ദേഹം](tg://user?id={}) നോക്കി നടത്തുന്ന ഒരു അടിപൊളി അഡ്മിൻ ബോട്ടാണ്.
 
-എന്നെ നിർമ്മിച്ചിരിക്കുന്നത് python3 യിൽ python-telegram-bot ലൈബ്രറി ഉപയോഗിച്ചാണ്. ഞാൻ പൂർണ്ണമായിട്ടും ഓപ്പൺസോഴ്സ്ഡ് ആണ്.
-
-എന്നെപ്പോലെ ഒരു അഡ്മിൻ ബോട്ടിനെ ഉണ്ടാക്കുവാൻ താഴെ കൊടുത്തിരിക്കുന്ന വീഡിയോ കാണുക.
-
-എന്റെ അപ്ഡേറ്റുകളെക്കുറിച്ചും പ്രവർത്തനത്തെപറ്റിയും അറിയുവാൻ അപ്ഡേറ്റ് ചാനൽ സബ്സ്ക്രൈബ് ചെയ്യുക.
-
-കൂടെ താഴെ കൊടുത്തിരിക്കുന്ന ടെക് ഗെയിമിംഗ് യൂട്യൂബ് ചാനൽ സബ്സ്ക്രൈബ് ചെയ്യാനും മറക്കേണ്ട.
+എന്നെ നിർമ്മിച്ചിരിക്കുന്നത് python3 യിൽ python-telegram-bot ലൈബ്രറി ഉപയോഗിച്ചാണ്.
 
 ലഭ്യമായ കമാന്റുകളെപ്പറ്റി അറിയുവാൻ /help അമർത്തുക.
 
@@ -36,7 +30,7 @@ PM_START_TEXT = """
 HELP_STRINGS = """
 Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
-the things I can help you with.
+the things I can help you with. my dev is @shamilnelli.
 *Main* commands available:
  - /start: start the bot
  - /help: PM's you this message.
@@ -150,9 +144,9 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
 
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="🎉 Add me to your group", url="t.me/{}?startgroup=true".format(bot.username)),  InlineKeyboardButton(text="🤖 Make Own Admin Bot", url="https://t.me/Keralamarket")],
-                     [InlineKeyboardButton(text="👥 Support Group", url="https://t.me/Keralamarket"), InlineKeyboardButton(text="🔔 Update Channel", url="https://t.me/Keralamarkets")],
-                     [InlineKeyboardButton(text="🎬 Youtube Channel", url="https://t.me/Keralamarket"), InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
+                    [[InlineKeyboardButton(text="🎉 Add me to your group", url="t.me/{}?startgroup=true".format(bot.username)),  InlineKeyboardButton(text="🤖 OWNER", url="https://t.me/arumillathavann")],
+                     [InlineKeyboardButton(text="👥 Support Group", url="https://t.me/redbullfed"), InlineKeyboardButton(text="🧑💻 Contact Dev", url="https://t.me/shamilnelli")],
+                     [InlineKeyboardButton(text="🎬 Report Issues", url="https://t.me/shamilhelpbot"), InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
 
     else:
         update.effective_message.reply_text("ചത്തിട്ടില്ലാ...")
